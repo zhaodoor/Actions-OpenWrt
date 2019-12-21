e600f58dc97b709e198864b05683f9d463d0a94e
lietxia <lietxia@gmail.com>
2019-12-21 20:28:40 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,30 +1,23 @@
+echo "keepalive..."
 touch /tmp/keepalive
+
 docker rmi `docker images -q`
 echo "Deleting files, please wait ..."
-sudo rm -rf \
-  /usr/share/dotnet \
-  /etc/mysql \
-  /etc/php
-sudo -E apt-get -y purge \
-  azure-cli \
-  ghc* \
-  zulu* \
-  hhvm \
-  llvm* \
-  firefox \
-  google* \
-  dotnet* \
-  powershell \
-  openjdk* \
-  mysql* \
-  php*
+
+sudo rm -rf /usr/share/dotnet /etc/mysql /etc/php /etc/apt/sources.list.d
+sudo -E apt-get -y purge azure-cli ghc* zulu* hhvm llvm* firefox google* dotnet* powershell openjdk* mysql* php*
+
+echo "apt install"
 sudo -E apt-get update
 sudo -E apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler
 sudo -E apt-get -y autoremove --purge
 sudo -E apt-get clean
+sudo ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
+
+echo "git clone"
 git clone https://github.com/coolsnowwolf/lede openwrt
 cd openwrt
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-echo "make menuconfig"
+make menuconfig
